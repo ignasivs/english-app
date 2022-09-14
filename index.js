e9fb6817337dbd50143ff0956b57a37874780cda
vicent.ignasi <ignacio.vicent@exoticca.com>
2022-09-14 15:14:32 +0200

    primera version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //require the just installed express app
 var express = require('express');
 var bodyParser = require("body-parser");
+var mysql = require('mysql');
 //then we call express
 var app = express();
 
@@ -8,38 +9,79 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-var task = ["buy socks", "practise with nodejs", "hola", "adios", "bien"];
-var complete = ["finish jquery"];
+let words = [];
+
+var connection = mysql.createConnection({
+    host     : 'remotemysql.com',
+    database : 'AUr9u7g0jl',
+    user     : 'AUr9u7g0jl',
+    password : '5Dytz7Tu3i',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected');
+});
+
+loadWords();
 
 //takes us to the root(/) URL
 app.get('/', function (req, res) {
-    res.render("index", { task: task, complete: complete });
+    res.render("index", { words });
 });
 app.get('/list', function (req, res) {
-    res.render("list", { task: task, complete: complete });
+    res.render("list", { words });
 });
-app.post('/addtask', function (req, res) {
+ app.post('/addword', async function (req, res) {
     var newName = req.body.newName;
     var newTranslation = req.body.newTranslation;
-    task.push(newName);
-    task.push(newTranslation);
+    await saveNewWord(newName, newTranslation);
+    words.push({name: newName, translation: newTranslation});
     res.redirect("/");
  });
- app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        //check if the completed task already exist in the task when checked, then remove using the array splice method
-        task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
+ app.post("/removeword", async function(req, res) {
+    let values = req.body.values.split(",");
+    const index = words.findIndex(val => {
+        return val.name == values[0] && val.translation == values[1];
+    });
+    words.splice(index, 1);
+    await deleteWord(values[0], values[1]);
+    //await loadWords();
     res.redirect("/list");
 });
 //the server is listening on port 3000 for connections
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+
+function saveNewWord(name, translation) {
+    let sql = `INSERT INTO words (name, translation) VALUES ('${name}', '${translation}')`;
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+      });
+}
+
+function deleteWord(name, translation) {
+    let sql = `DELETE FROM words WHERE name = '${name}' and translation = '${translation}'`;
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Number of records deleted: " + result.affectedRows);
+      });
+}
+
+function loadWords() {
+    words = [];
+    connection.query('SELECT * FROM words', function (error, results, fields) {
+        if (error)
+            throw error;
+    
+        results.forEach(result => {
+            words.push({name: result.name, translation: result.translation});
+        });
+    });
+}
